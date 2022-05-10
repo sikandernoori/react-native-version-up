52fcac0b11b59dfbeaf7f40ed69b3a419c86394a
Skandar Munir Ahmed <sikandernoori@gmail.com>
2022-05-10 14:13:46 +0500

    customed to github run number and extraas removed

--- index.js
+++ index.js
@@ -26,13 +26,14 @@ let minor = helpers.version(versions[1], argv.minor, argv.major);
 let patch = helpers.version(versions[2], argv.patch, argv.major || argv.minor);
 const version = `${major}.${minor}.${patch}`;
 
+log.info(`- runNumber (${argv.runNumber});`, 1);
+
+
 // getting next build number
 const buildCurrent = helpers.getBuildNumberFromPlist(pathsToPlists[0]);
-const build = ('000' + major).slice(-3) + ('000' + minor).slice(-3) + ('000' + patch).slice(-3) + ('000' + argv.runNumber % 1000 + patch).slice(-3);
+const build = major + ('000' + minor).slice(-3) + ('000' + patch).slice(-3) + ('000' + argv.runNumber).slice(-3);
 
-// getting commit message
-const messageTemplate = argv.m || argv.message || 'release ${version}: increase versions and build numbers';
-const message = messageTemplate.replace('${version}', version);
+const message = version;
 
 log.info('\nI\'m going to increase the version in:');
 log.info(`- package.json (${pathToPackage});`, 1);
@@ -43,10 +44,6 @@ log.notice(`\nThe version will be changed:`);
 log.notice(`- from: ${versionCurrent} (${buildCurrent});`, 1);
 log.notice(`- to:   ${version} (${build}).`, 1);
 
-if (version === versionCurrent) {
-  log.warning('\nNothing to change in the version. Canceled.');
-  process.exit();
-}
 
 
 const chain = new Promise((resolve, reject) => {
@@ -57,18 +54,12 @@ const chain = new Promise((resolve, reject) => {
   }
 
   const question = log.info(`Use "${version}" as the next version? [y/n] `, 0, true);
-  const answer = readlineSync.question(question).toLowerCase();
-  answer === 'y' ? resolve() : reject('Process canceled.');
+  resolve()
 });
 
 
 const update = chain.then(() => {
   log.notice('\nUpdating versions');
-}).then(() => {
-  log.info('Updating version in package.json...', 1);
-
-  helpers.changeVersionInPackage(pathToPackage, version);
-  log.success(`Version in package.json changed.`, 2);
 }).then(() => {
   log.info('Updating version in xcode project...', 1);
 
@@ -83,31 +74,3 @@ const update = chain.then(() => {
   log.success(`Version and build number in android project (gradle file) changed.`, 2);
 });
 
-const commit = update.then(() => {
-  log.notice(`\nI'm ready to cooperate with the git!`);
-  log.info('I want to make a commit with message:', 1);
-  log.info(`"${message}"`, 2);
-  log.info(`I want to add a tag:`, 1);
-  log.info(`"v${version}"`, 2);
-
-  const question = log.info(`Do you allow me to do this? [y/n] `, 1, true);
-  const answer = readlineSync.question(question).toLowerCase();
-  if (answer === 'y') {
-    return helpers.commitVersionIncrease(version, message, [
-      pathToPackage,
-      ...pathsToPlists,
-      pathToGradle
-    ]).then(() => {
-      log.success(`Commit with files added. Run "git push".`, 1);
-    });
-  } else {
-    log.warning(`Skipped.`, 1);
-  }
-});
-
-commit.then(() => {
-  log.success(`\nDone!`);
-}).catch(e => {
-  log.line();
-  log.error(e)
-});
